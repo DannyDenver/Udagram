1f4993ede74933f058ae04697547693b2ae0d1bf
DannyDenver <djt09d@fulbrightmail.org>
2019-12-10 14:24:48 -0700

    init app with connections to AWS RDS and S3

---
+++ src/config/config.ts
@@ -0,0 +1,26 @@
+export const config = {
+  "postgres": {
+    "username": process.env.POSTGRESS_USERNAME,
+    "password": process.env.POSTGRESS_PASSWORD,
+    "database": process.env.POSTGRESS_DATABASE,
+    "host": process.env.POSTGRESS_HOST,
+    "dialect": "postgres"
+  },
+  "dev": {
+    "username": process.env.POSTGRESS_USERNAME,
+    "password": process.env.POSTGRESS_PASSWORD,
+    "database": process.env.POSTGRESS_DATABASE,
+    "host": process.env.POSTGRESS_HOST,
+    "dialect": "postgres",
+    "aws_region": process.env.FILESTORE_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
+    "aws_media_bucket": process.env.FILESTORE_MEDIA_BUCKET
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  }
+}
